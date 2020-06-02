45c3cad9a6cd4040c9dfb310e34f6ed04b5d2610
Daniel Alejandro Rodriguez Gomez <daalrodr@bancolombia.com.co>
2020-06-01 22:55:20 -0500

    Implementacion basica de balanceo de cargas con Ribbon usando RestTemplate

--- springboot-app-productos/src/main/java/co/daniel/springboot/app/productos/controllers/ProductoController.java
+++ springboot-app-productos/src/main/java/co/daniel/springboot/app/productos/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package co.daniel.springboot.app.productos.controllers;
 import co.daniel.springboot.app.productos.models.entity.Producto;
 import co.daniel.springboot.app.productos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +18,16 @@ public class ProductoController {
     @Autowired
     private Environment environment;
 
+    @Value("${server.port}")
+    private Integer port;
+
     @Autowired
     private IProductoService productoService;
 
     @GetMapping("/listar")
     public List<Producto> listar() {
         return productoService.findAll().stream().map(p -> {
-                    p.setPort(Long.parseLong(environment.getProperty("local.server.port")));
+                    p.setPort(port.longValue());
                     return p;
         }
         ).collect(Collectors.toList());
@@ -32,7 +36,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id){
         Producto producto =productoService.findById(id);
-        producto.setPort(Long.parseLong(environment.getProperty("local.server.port")));
+        //producto.setPort(Long.parseLong(environment.getProperty("local.server.port")));
+        producto.setPort(port.longValue());
         return producto;
     }
 }
